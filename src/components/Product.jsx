2f16514f4aa247a9e122cfcd77674358213de819
Sagnik Chakraborty <sagnikchk118@gmail.com>
2024-10-23 00:43:19 +0530

    Updated product design

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,18 +3,22 @@ import { priceFormatter } from '../utils/price-formatter';
 
 function Product({ image, title, price, description }) {
   return (
-    <article className="w-full h-full rounded-lg bShadow">
-      <img className="w-full h-1/2 rounded-t-lg" src={image} alt={title} />
-      <div className="h-1/2 flex flex-col justify-between rounded-b-lg">
-        <div className="flex flex-col gap-y-2 px-4 py-3">
-          <p className="font-merriweather font-bold text-xl">{title}</p>
-          <p className="font-merriweather font-semibold text-lg">
+    <article className="flex flex-col bShadow rounded-lg w-full h-full">
+      <img className="w-full rounded-t-lg" src={image} alt={title} />
+      <div className="flex flex-col h-full justify-between rounded-b-lg bg-slate-100">
+        <div className="px-4 py-2">
+          <p className="my-2 font-merriweather font-bold text-xl">{title}</p>
+          <p className="my-2 font-merriweather font-semibold text-lg text-red-700">
             {priceFormatter.format(price)}
           </p>
-          <p className="font-quicksand font-semibold text-md text-ellipsis overflow-hidden">{description}</p>
+          <p className="my-2 max-h-[25vh] font-quicksand font-semibold overflow-hidden text-ellipsis">
+            {description}
+          </p>
         </div>
-        <div>
-          <button>Add to Cart</button>
+        <div className="m-2 text-right">
+          <button className="w-10 h-10 rounded-full font-quicksand font-bold text-xl bg-amber-400 truncate active:-translate-x-0.5 active:translate-y-0.5">
+            +
+          </button>
         </div>
       </div>
     </article>
